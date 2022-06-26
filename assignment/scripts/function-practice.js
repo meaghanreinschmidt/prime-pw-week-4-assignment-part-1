bfc79d222afbe1d831d7bada16f7aaf330bb9a91
Meaghan Reinschmidt <mreinschmidt@hotmail.com>
2022-06-25 21:44:39 -0500

    added code comments to questions 1-8

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -14,6 +14,11 @@ console.log('Test - should say "Hello World!"', hello());
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
+/**
+ * Will log specific name to console
+ * @param {string} name Specified name will be called
+ * @returns Returns a name 
+ */
 function helloName( name ) {
   return 'Maddie';
 }
@@ -21,16 +26,26 @@ function helloName( name ) {
 console.log('Hello,', helloName() + '!');
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber, secondNumber) {
-  firstNumber = 29;
-  secondNumber = 3;
-  return 29 + 3;
+/**
+ * Adds two numbers together and returns outcome 
+ * @param {number} firstNumber 
+ * @param {number} secondNumber 
+ * @returns Returns the added numbers 
+ */
+function addNumbers(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
   // return firstNumber + secondNumber;
 }
-console.log('The added numbers equal:', addNumbers());
-//////////////////////////////////////////////////////////What format to use for return???///////////
+console.log('The added numbers equal:', addNumbers(29, 3));
 
 // 4. Function to multiply three numbers & return the result
+/**
+ * Multiplies three numbers and returns outcome
+ * @param {number} firstNumber 
+ * @param {number} secondNumber 
+ * @param {number} thirdNumber 
+ * @returns Returns the multiplied numbers 
+ */
 function multiplyThree( firstNumber, secondNumber, thirdNumber){
   return 42 * 16 * 1;
 }
@@ -38,6 +53,11 @@ console.log('The multiplied numbers equal:', multiplyThree());
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
+/**
+ * Takes in a number and returns true if positve or > 0 or false if negative
+ * @param {number} number 
+ * @returns Returns a true or false depending on if the number if positive or negative
+ */
 function isPositive( number ) {
   if ( number > 0 ){
     return true;
@@ -49,16 +69,20 @@ function isPositive( number ) {
 console.log('True or False?', isPositive(17));
 // Write a separate console.log statement for each outcome
 console.log( 'isPositive - should say true', isPositive(3) );
-console.log('3 is greater than 0:', isPositive(3));
 console.log( 'isPositive - should say false', isPositive(0) );
-console.log('0 is greater than 0:', isPositive(0));
 console.log( 'isPositive - should say false', isPositive(-3) );
-console.log('-3 is greater than 0:', isPositive(-3));
-// ADD MATH? ////////////////////////////////////////////////////////////////////////////////////////
+console.log('What happens when I use math?', isPositive(17 + 9));
+console.log('What happens if I use math again?', isPositive(24 - 53));
+console.log('Let\'s try math one more time!', isPositive(53 * 0));
+
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
-
+/**
+ * Takes in an array and returns the last item in the array 
+ * @param {string} hungryArray an array of many items
+ * @returns Returns the last item in the array or 'undefined' if there are no items
+ */
 function getLast(hungryArray= ['Pizza', 'Bagels', 'Pepperoni', 'Bagel Bites']) {
   if (hungryArray.length >= 0) {
     let theLastItem = hungryArray[hungryArray.length - 1];
@@ -73,17 +97,22 @@ console.log(getLast());
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
+/**
+ * Takes in a value and finds it in the array
+ * @param {number} value a number that may or may not be in the array
+ * @param {number} differentArray an array of numbers
+ * @returns Returns true if the value is in the array or false if the value is not in the array
+ */
 
-// function find(value, differentArray) {
-//   for(let i = 0; i < differentArray.length; i++) {
-//     if () { // Don't use Array.includes, Array.indexOf, or Array.find/////////////////////////////
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   }
-// }
-// console.log('Testing:', find(3, [4, 5, 6, 7]));
+function find(value, differentArray) {
+  for(let i = 0; i < differentArray.length; i++) {
+    if (value === differentArray[i]) { 
+      return true;
+  }
+}
+return false;
+}
+console.log('Testing:', find(0, [4, 5, 6, 7]));
 
 
 // ----------------------
@@ -91,6 +120,12 @@ console.log(getLast());
 // ----------------------
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
+/**
+ * Takes in a letter and finds it as the first letter in the string
+ * @param {letter} letter a letter that may or may not be the first letter in the string
+ * @param {string} string 
+ * @returns Returns true if the letter matches the first letter of the string or false if the letter does not match the first letter in the string
+ */
 function isFirstLetter(letter, string) {
  if(letter === string.charAt(0)) {
   return true;
@@ -102,13 +137,13 @@ console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll() {
-  
-  let sum = 0
+// function sumAll() {
 
-  // TODO: loop to add items
-  return sum;
-}
+//   let sum = 0
+
+//   // TODO: loop to add items
+//   return sum;
+// }
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
@@ -118,4 +153,4 @@ function sumAll() {
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
-//     here in a comment, write the function, and test it!
+//     here in a comment, write the function, and test it
